211b7929ea7918da996f71018da9711e514cff88
김민지 : ) <happyhill99@gmail.com>
2022-01-27 16:44:19 +0900

    .

--- 서울에서 김서방 찾기.py
+++ 서울에서 김서방 찾기.py
@@ -3,3 +3,9 @@ def solution(seoul):
         if seoul[i]=="Kim":
             answer="김서방은"+str[i]+"에 있다."
     return answer
+
+def solution(seoul):
+    for i in range(len(seoul)):
+        if seoul[i]=="Kim":
+            answer="김서방은 {}에 있다.".format(i)
+    return answer
--- 시저 암호.py
+++ 시저 암호.py
@@ -1,4 +1,12 @@
 def solution(s, n):
-    
+    a='abcedfghijklmnopqrstuvwxyz'
+    A='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     answer = ''
+    for i in s:
+        if i in a:
+            answer += a[(a.index(i)+n)%26]
+        elif i in A:
+            answer += A[(A.index(i)+n)%26]
+        else:
+            answer += " "
     return answer
