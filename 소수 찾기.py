dd914bab3081bc64956d3cb240510494894d322a
김민지 : ) <happyhill99@gmail.com>
2022-01-27 15:10:14 +0900

    .

---
+++ 소수 찾기.py
@@ -0,0 +1,7 @@
+def solution(n):
+    answer = 0
+    a=''
+    for i in range(2,n+1):
+        if i=2:
+               
+    return answer
---
+++ 수박수박수박수박수?.py
@@ -0,0 +1,20 @@
+def solution(n):
+    answer = ''
+    a=''
+    b=''
+    c=''
+    for i in range(n):
+        if i%2==0:
+            a="수"
+            answer+=str(a)
+        elif i==0:
+            b="수"
+            answer+=str(b)
+        else:
+            c="박"
+            answer+=str(c)
+    return answer
+
+#다른 사람 풀이
+def solution(n): 
+    return ('수박'*n) [:n]
